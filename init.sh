1c9dd3a85e57fd76fb69f760c9803d76dfb91bc8
Assaf Lavie <me@assaflavie.com>
2015-12-21 16:51:08 +0200

    Fixed grafana init script (added django syncdb)

--- init.sh
+++ init.sh
@@ -15,4 +15,5 @@ touch /opt/graphite/storage/graphite.db /opt/graphite/storage/index
 chown -R www-data /opt/graphite/storage
 chmod 0775 /opt/graphite/storage /opt/graphite/storage/whisper
 chmod 0664 /opt/graphite/storage/graphite.db
-usr/bin/supervisord
+cd /opt/graphite/webapp/graphite && python manage.py syncdb --noinput
+/usr/bin/supervisord
